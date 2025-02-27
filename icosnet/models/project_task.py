1f81d9ac26f4f106104bc515e4de53e9b6e3a7f8
Abdelghani BENYAMINA DOUMA <abgh.bend98@gmail.com>
2025-02-27 12:21:22 +0100

    Fix report css

--- icosnet/models/project_task.py
+++ icosnet/models/project_task.py
@@ -6,11 +6,16 @@ from odoo import api, models, fields, _, exceptions
 class Task(models.Model):
     _inherit = "project.task"
 
-
+    order_partner_id = fields.Many2one('res.partner', string="Client", compute='_compute_partner_id')
+    order_id = fields.Many2one('sale.order', related="sale_line_id.order_id")
 
     state = fields.Selection(selection_add=[
             ('5_disapproved', 'Défavorable'),
         ] , ondelete={'5_disapproved': 'set default'})
+
+    def _compute_partner_id(self):
+        for rec in self:
+            rec.order_partner_id = rec.sale_line_id.order_id.partner_id
    
     def action_open_fiche_technique(self):
         self.ensure_one()
